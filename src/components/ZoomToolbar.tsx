b784380ee7dbebf6cf4cd23852c5fc46fc4a523b
Rubem Neto <rubemviscard2635@gmail.com>
2025-08-23 23:53:36 +0000

    chore: add download button to pdfviewer

---
+++ src/components/ZoomToolbar.tsx
@@ -0,0 +1,39 @@
+import { useZoom } from '@embedpdf/plugin-zoom/react';
+import { useExportCapability } from '@embedpdf/plugin-export/react'
+import { Plus } from 'lucide-react'
+import { Minus } from 'lucide-react'
+import { Download } from 'lucide-react'
+
+export const ZoomToolbar = () => {
+	const { provides: zoomProvides, state: zoomState } = useZoom();
+	const { provides: exportProvider } = useExportCapability()
+
+
+
+	if (!zoomProvides) {
+		return null;
+	}
+
+
+	return (
+		<div className='flex justify-between border p-3 border-dashed rounded'>
+			<div className='flex gap-2'>
+				<span>Zoom: {Math.round(zoomState.currentZoomLevel * 100)}%</span>
+				<div className='flex justify-center' >
+					<button onClick={zoomProvides.zoomOut} className='hover:text-[#b95e3f] hover:cursor-pointer'>
+						<Minus />
+					</button>
+					<button onClick={zoomProvides.zoomIn} className='hover:text-[#b95e3f] hover:cursor-pointer'>
+						<Plus />
+					</button>
+				</div>
+			</div>
+			<button
+				onClick={exportProvider?.download}
+				className='hover:text-[#b95e3f] hover:cursor-pointer'
+			>
+				<Download />
+			</button>
+		</div >
+	);
+};
