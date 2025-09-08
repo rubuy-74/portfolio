19a5509db2ec2966bacf66353f5609a270ffdeee
Rubem Neto <rubemviscard2635@gmail.com>
2025-09-08 21:46:50 +0000

    chore: add defineConfig import path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind'; // <-- Import the new integration
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
