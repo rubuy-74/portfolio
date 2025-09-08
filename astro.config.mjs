6c440b9ff09c18df488a68fca42ed832ddec3f6c
Rubem Neto <rubemviscard2635@gmail.com>
2025-09-08 21:44:21 +0000

    fix: tailwind dependency & vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,32 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
-
-import vercel from '@astrojs/vercel';
-
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind'; // <-- Import the new integration
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-    server: {
-      proxy: {
-        '/api/check': {
-          target: 'https://pstr-production.up.railway.app',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-        },
-      },
-    },
-  },
+	site: 'https://rubuy.me',
+	vite: {
+		server: {
+			proxy: {
+				'/api/check': {
+					target: 'https://pstr-production.up.railway.app',
+					changeOrigin: true,
+					rewrite: (path) => path.replace(/^\/api/, ''),
+				},
+			},
+		},
+	},
 
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-    maxDuration: 8
-  }),
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+		maxDuration: 8
+	}),
 
-  integrations: [react()]
+	integrations: [
+		react(),
+		tailwind({
+			configFile: './tailwind.config.js'
+		})
+	]
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte,md,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "#f8f9fa", // Replace with your desired color
-        primary: "#212529",   // Replace with your desired color
-        secondary: "#6c757d", // Replace with your desired color
-        accent: "#0d6efd",    // Replace with your desired color
-      }
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte,md,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: "#f8f9fa",
+				primary: "#212529",
+				secondary: "#6c757d",
+				accent: "#0d6efd",
+			}
+		},
+	},
+	plugins: [],
 }
